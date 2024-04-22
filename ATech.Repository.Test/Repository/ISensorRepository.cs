15dc507f077c363a4ffbd2a5882eed4772580d62
maurizio.attanasi <maurizio.attanasi@gmail.com>
2024-04-22 22:57:09 +0200

    Update repository interfaces and implementations to include primary key type parameter
    - Update `IIoTDataMartDbUnitOfWork` interface and its implementation to remove unnecessary usings
    - Update `IMeasureRepository`, `IPhys

--- ATech.Repository.Test/Repository/ISensorRepository.cs
+++ ATech.Repository.Test/Repository/ISensorRepository.cs
@@ -2,7 +2,7 @@ using ATech.Repository.Test.Entities;
 
 namespace ATech.Repository.Test.Repository
 {
-    public interface ISensorRepository : IRepository<Sensor>
+    public interface ISensorRepository : IRepository<Sensor, long>
     {
     }
 }
