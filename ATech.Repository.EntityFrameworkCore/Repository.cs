151c31020616192529f479a7ce2e1a573cec0948
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-04-23 09:38:06 +0200

    Update methods completed

--- ATech.Repository.EntityFrameworkCore/Repository.cs
+++ ATech.Repository.EntityFrameworkCore/Repository.cs
@@ -15,47 +15,48 @@ namespace ATech.Repository.EntityFrameworkCore
             => _context = context;
 
         public virtual TEntity Get(TId id) 
-            => this._context.Set<TEntity>().Find(id);
+            => _context.Set<TEntity>().Find(id);
 
         public virtual async ValueTask<TEntity> GetAsync(TId id, CancellationToken cancellationToken) 
             => await _context.Set<TEntity>().FindAsync(new object[] { id }, cancellationToken);
 
         public virtual TEntity Get(Guid id) 
-            => this._context.Set<TEntity>().Find(id);
+            => _context.Set<TEntity>().Find(id);
 
         public virtual IEnumerable<TEntity> GetAll() 
-            => this._context.Set<TEntity>().ToList();
+            => _context.Set<TEntity>().ToList();
 
         public virtual async ValueTask<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken) 
-            => await this._context.Set<TEntity>()
-                    .ToListAsync(cancellationToken);
+            => await _context.Set<TEntity>().ToListAsync(cancellationToken);
 
         public virtual IEnumerable<TEntity> Find(Func<TEntity, bool> predicate) 
-            => this._context.Set<TEntity>()
-                .Where(predicate);
+            => _context.Set<TEntity>().Where(predicate);
 
         public virtual void Add(TEntity entity) 
-            => this._context.Set<TEntity>().Add(entity);
+            => _context.Set<TEntity>().Add(entity);
 
         public virtual async ValueTask AddAsync(TEntity entity, CancellationToken cancellationToken) 
-            => await this._context.Set<TEntity>().AddAsync(entity, cancellationToken);
+            => await _context.Set<TEntity>().AddAsync(entity, cancellationToken);
 
         public virtual void AddRange(IEnumerable<TEntity> entities) 
-            => this._context.Set<TEntity>().AddRange(entities);
+            => _context.Set<TEntity>().AddRange(entities);
 
         public virtual void Remove(TEntity entity) 
-            => this._context.Set<TEntity>().Remove(entity);
+            => _context.Set<TEntity>().Remove(entity);
 
         public virtual void RemoveRange(IEnumerable<TEntity> entities) 
-            => this._context.Set<TEntity>().RemoveRange(entities);
+            => _context.Set<TEntity>().RemoveRange(entities);
 
         public int Count() 
-            => this._context.Set<TEntity>().Count();
+            => _context.Set<TEntity>().Count();
 
         public void Update(TEntity entity) 
-            => throw new NotImplementedException();
+            => _context.Set<TEntity>().Update(entity);
 
-        public ValueTask UpdateAsync(TEntity entity, CancellationToken cancellationToken) 
-            => throw new NotImplementedException();
+        public ValueTask UpdateAsync(TEntity entity, CancellationToken cancellationToken) {
+            _context.Set<TEntity>().Update(entity);
+            return Task.CompletedTask;
+        }
+            
     }
 }
