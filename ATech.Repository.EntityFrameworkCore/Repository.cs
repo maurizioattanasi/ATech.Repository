3dc4db7b72f2420064e378871b080f0b5f1826e8
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-05-04 09:42:35 +0200

    Cleanup

--- ATech.Repository.EntityFrameworkCore/Repository.cs
+++ ATech.Repository.EntityFrameworkCore/Repository.cs
@@ -5,56 +5,64 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace ATech.Repository.EntityFrameworkCore
+namespace ATech.Repository.EntityFrameworkCore;
+
+public class Repository<TEntity, TId> : IRepository<TEntity, TId> where TEntity : class
 {
-    public class Repository<TEntity, TId> : IRepository<TEntity, TId> where TEntity : class
+    public readonly DbContext _context;
+
+    public Repository(DbContext context)
+        => _context = context;
+
+    public virtual TEntity? Get(TId id)
+        => _context.Set<TEntity>().Find(id);
+
+    public virtual async ValueTask<TEntity?> GetAsync(TId id, CancellationToken cancellationToken)
+        => await _context.Set<TEntity>().FindAsync(new object[] { id }, cancellationToken);
+
+    public virtual IEnumerable<TEntity> GetAll()
+        => _context.Set<TEntity>().ToList();
+
+    public virtual async ValueTask<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+        => await _context.Set<TEntity>().ToListAsync(cancellationToken);
+
+    public virtual IEnumerable<TEntity> Find(Func<TEntity, bool> predicate)
+        => _context.Set<TEntity>().Where(predicate);
+
+    public IEnumerable<TEntity> Missing(IEnumerable<TEntity> toExclude, IEqualityComparer<TEntity>? comparer)
+        => toExclude.Except(_context.Set<TEntity>(), comparer);
+
+    public virtual void Add(TEntity entity)
+        => _context.Set<TEntity>().Add(entity);
+
+    public virtual async ValueTask AddAsync(TEntity entity, CancellationToken cancellationToken)
+        => await _context.Set<TEntity>().AddAsync(entity, cancellationToken);
+
+    public virtual void AddRange(IEnumerable<TEntity> entities)
+        => _context.Set<TEntity>().AddRange(entities);
+
+    public virtual async ValueTask AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
     {
-        public readonly DbContext _context;
-
-        public Repository(DbContext context) 
-            => _context = context;
-
-        public virtual TEntity Get(TId id) 
-            => _context.Set<TEntity>().Find(id);
-
-        public virtual async ValueTask<TEntity> GetAsync(TId id, CancellationToken cancellationToken) 
-            => await _context.Set<TEntity>().FindAsync(new object[] { id }, cancellationToken);
-
-        public virtual TEntity Get(Guid id) 
-            => _context.Set<TEntity>().Find(id);
-
-        public virtual IEnumerable<TEntity> GetAll() 
-            => _context.Set<TEntity>().ToList();
-
-        public virtual async ValueTask<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken) 
-            => await _context.Set<TEntity>().ToListAsync(cancellationToken);
-
-        public virtual IEnumerable<TEntity> Find(Func<TEntity, bool> predicate) 
-            => _context.Set<TEntity>().Where(predicate);
-
-        public virtual void Add(TEntity entity) 
-            => _context.Set<TEntity>().Add(entity);
-
-        public virtual async ValueTask AddAsync(TEntity entity, CancellationToken cancellationToken) 
-            => await _context.Set<TEntity>().AddAsync(entity, cancellationToken);
-
-        public virtual void AddRange(IEnumerable<TEntity> entities) 
-            => _context.Set<TEntity>().AddRange(entities);
-
-        public virtual void Remove(TEntity entity) 
-            => _context.Set<TEntity>().Remove(entity);
-
-        public virtual void RemoveRange(IEnumerable<TEntity> entities) 
-            => _context.Set<TEntity>().RemoveRange(entities);
-
-        public int Count() 
-            => _context.Set<TEntity>().Count();
-
-        public void Update(TEntity entity) 
-            => _context.Set<TEntity>().Update(entity);
-
-        public async ValueTask UpdateAsync(TEntity entity, CancellationToken cancellationToken) 
-            => await Task.Run(() => _context.Set<TEntity>().Update(entity), cancellationToken);
-
+        _context.Set<TEntity>().AddRange(entities);
+        await Task.Yield();
     }
+
+    public virtual void Remove(TEntity entity)
+        => _context.Set<TEntity>().Remove(entity);
+
+    public virtual void RemoveRange(IEnumerable<TEntity> entities)
+        => _context.Set<TEntity>().RemoveRange(entities);
+
+    public int Count()
+        => _context.Set<TEntity>().Count();
+
+    public void Update(TEntity entity)
+        => _context.Set<TEntity>().Update(entity);
+
+    public async ValueTask UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+        => await Task.Run(() => _context.Set<TEntity>().Update(entity), cancellationToken);
+
+    public int SaveChanges() => _context.SaveChanges();
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => await _context.SaveChangesAsync(cancellationToken);
 }
--- ATech.Repository/IRepository.cs
+++ ATech.Repository/IRepository.cs
@@ -7,26 +7,33 @@ namespace ATech.Repository;
 
 public interface IRepository<TEntity, TId>
 {
-    TEntity Get(TId id);
-    ValueTask<TEntity> GetAsync(TId id, CancellationToken cancellationToken);
+    TEntity? Get(TId id);
+    ValueTask<TEntity?> GetAsync(TId id, CancellationToken cancellationToken);
 
     IEnumerable<TEntity> GetAll();
     ValueTask<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
 
     IEnumerable<TEntity> Find(Func<TEntity, bool> predicate);
 
+    IEnumerable<TEntity> Missing(IEnumerable<TEntity> toExclude, IEqualityComparer<TEntity>? comparer);
+
     void Add(TEntity entity);
     ValueTask AddAsync(TEntity entity, CancellationToken cancellationToken);
 
     void AddRange(IEnumerable<TEntity> entities);
+    ValueTask AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
 
     void Update(TEntity entity);
 
-    ValueTask    UpdateAsync(TEntity entity, CancellationToken cancellationToken);
+    ValueTask UpdateAsync(TEntity entity, CancellationToken cancellationToken);
 
     void Remove(TEntity entity);
 
     void RemoveRange(IEnumerable<TEntity> entities);
 
     int Count();
+
+    int SaveChanges();
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
