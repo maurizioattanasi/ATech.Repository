a35d578c6a44fd49d40f137b28e92473984e0626
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2025-01-28 13:39:35 +0100

    minor fixes

--- src/ATech.Repository/ExpressionExtensions.cs
+++ src/ATech.Repository/ExpressionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq.Expressions;
 
 namespace ATech.Repository;
@@ -6,8 +7,8 @@ public static class ExpressionExtensions
 {
     public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
     {
-        var parameter = Expression.Parameter(typeof(T));
-        var body = Expression.AndAlso(
+        ParameterExpression parameter = Expression.Parameter(typeof(T));
+        BinaryExpression body = Expression.AndAlso(
             Expression.Invoke(expr1, parameter),
             Expression.Invoke(expr2, parameter)
         );
@@ -16,8 +17,8 @@ public static class ExpressionExtensions
 
     public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
     {
-        var parameter = Expression.Parameter(typeof(T));
-        var body = Expression.OrElse(
+        ParameterExpression parameter = Expression.Parameter(typeof(T));
+        BinaryExpression body = Expression.OrElse(
             Expression.Invoke(expr1, parameter),
             Expression.Invoke(expr2, parameter)
         );
