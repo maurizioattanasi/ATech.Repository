1fe7691124175093abee8a8d6c36459453e675b2
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2025-01-28 11:37:41 +0100

    Added expression extensions

---
+++ src/ATech.Repository/ExpressionExtensions.cs
@@ -0,0 +1,26 @@
+using System.Linq.Expressions;
+
+namespace ATech.Repository;
+
+public static class ExpressionExtensions
+{
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+        var body = Expression.AndAlso(
+            Expression.Invoke(expr1, parameter),
+            Expression.Invoke(expr2, parameter)
+        );
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+        var body = Expression.OrElse(
+            Expression.Invoke(expr1, parameter),
+            Expression.Invoke(expr2, parameter)
+        );
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+}
